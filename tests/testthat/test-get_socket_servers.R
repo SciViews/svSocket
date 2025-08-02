ecbc62b692d31b36f036af90b6ffd4a17d07179c
Philippe Grosjean <phgrosjean@sciviews.org>
2025-08-02 12:17:15 +0200

    Two tests added

--- tests/testthat/test-get_socket_servers.R
+++ tests/testthat/test-get_socket_servers.R
@@ -1,3 +1,5 @@
 test_that("get_socket_servers() lists nothing upon package loading", {
-  expect_true(TRUE)
+  # No socket server, no clients
+  expect_null(get_socket_servers())
+  expect_null(get_socket_clients())
 })
